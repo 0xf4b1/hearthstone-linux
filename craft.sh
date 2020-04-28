9b5d2e43d9204342daf7a6ebf1a8ebd7ed836bdf
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2020-04-28 22:08:13 +0200

    Added craft.sh

---
+++ craft.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+HS_PATH=$1
+UNITY_PATH=$2/Hub/Editor/2018.4.10f1/Editor/Data/PlaybackEngines/LinuxStandaloneSupport/Variations/linux64_withgfx_nondevelopment_mono
+TARGET_PATH=$3
+
+echo "Copy game data files ..."
+
+cp -r $HS_PATH/Data $TARGET_PATH
+cp -r $HS_PATH/Strings $TARGET_PATH
+
+mkdir $TARGET_PATH/Bin
+cp -r $HS_PATH/Hearthstone.app/Contents/Resources/Data $TARGET_PATH/Bin/Hearthstone_Data
+cp $HS_PATH/Hearthstone.app/Contents/Resources/'unity default resources' $TARGET_PATH/Bin/Hearthstone_Data/Resources
+
+echo "Copy engine files ..."
+
+cp $UNITY_PATH/LinuxPlayer $TARGET_PATH/Bin/Hearthstone.x86_64
+cp -r $UNITY_PATH/Data/Mono $TARGET_PATH/Bin/Hearthstone_Data
+
+echo "Creating stubs ..."
+
+cp client.config $TARGET_PATH
+
+mkdir -p $TARGET_PATH/Bin/Hearthstone_Data/Mono/x86_64/lib/System/Library/Frameworks/CoreFoundation.framework
+
+make -C stubs
+
+cp stubs/CoreFoundation.so $TARGET_PATH/Bin/Hearthstone_Data/Mono/x86_64/lib/System/Library/Frameworks/CoreFoundation.framework
+cp stubs/libOSXWindowManagement.so $TARGET_PATH/Bin/Hearthstone_Data/Mono/x86_64
+
+make -C token
+
+cp token/Token.exe $TARGET_PATH
+
+echo "Done. Now generate your web token, before launching the game!"
