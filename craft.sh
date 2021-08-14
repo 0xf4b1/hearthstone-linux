39e7abc4b1ebf669bfb8c3a0740c78c00d74391d
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2021-08-15 01:42:15 +0200

    Add desktop entry

--- craft.sh
+++ craft.sh
@@ -4,7 +4,7 @@ set -e
 
 HS_PATH=$1
 UNITY_PATH=$2/Hub/Editor/2018.4.10f1/Editor/Data/PlaybackEngines/LinuxStandaloneSupport/Variations/linux64_withgfx_nondevelopment_mono
-TARGET_PATH=$3
+TARGET_PATH=$(realpath $3)
 
 mkdir -p $TARGET_PATH
 
@@ -16,6 +16,7 @@ cp -r $HS_PATH/Strings $TARGET_PATH
 mkdir $TARGET_PATH/Bin
 cp -r $HS_PATH/Hearthstone.app/Contents/Resources/Data $TARGET_PATH/Bin/Hearthstone_Data
 cp $HS_PATH/Hearthstone.app/Contents/Resources/'unity default resources' $TARGET_PATH/Bin/Hearthstone_Data/Resources
+cp $HS_PATH/Hearthstone.app/Contents/Resources/PlayerIcon.icns $TARGET_PATH/Bin/Hearthstone_Data/Resources
 
 echo "Copy engine files ..."
 
@@ -40,5 +41,14 @@ cp token/Token.exe $TARGET_PATH
 make -C login
 
 cp login/login $TARGET_PATH
+
+cat << EOF > ~/.local/share/applications/hearthstone.desktop
+[Desktop Entry]
+Type=Application
+Name=Hearthstone
+Exec=$TARGET_PATH/Bin/Hearthstone.x86_64
+Icon=$TARGET_PATH/Bin/Hearthstone_Data/Resources/PlayerIcon.icns
+Categories=Game;
+EOF
 
 echo "Done. Now generate your web token, before launching the game!"
